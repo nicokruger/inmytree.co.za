84544139240326ae6770bbc923c94fe821dec2b7
Nico <nico.kruger@gmail.com>
2021-04-05 08:00:43 +0200

    moar posts

--- app/.vitepress/posts.ts
+++ app/.vitepress/posts.ts
@@ -6,18 +6,44 @@ export default [
         date: '2021-03-31',
         excerpt: 'We recently redesigned our pricing algorithm to run AWS native. Find out how we are seeing up to a 100x speed improvement on training times.',
         status: 'published',
-        readMins: 20,
+        readMins: 8,
         tags: [ 'aws', 'ml', 'digitata']
     },
+
+    {
+        url: 'https://aws.amazon.com/blogs/machine-learning/how-digitata-provides-intelligent-pricing-on-mobile-data-and-voice-with-amazon-lookout-for-metrics/',
+        link:true,
+        title: 'Find out how Lookout for Metrics is helping Digitata provide an even better pricing optimisation solution to our customers',
+        publishDate: '30th March 2021',
+        date: '2021-03-30',
+        excerpt: 'A guest AWS post by myself in conjunction with Chris King from AWS. As William Gibson said, "The future is here, it\'s just not evenly distributed". Find out how this new AWS service is helping us to get more people connected at better prices.',
+        status: 'published',
+        readMins: 10,
+        tags: [ 'aws', 'ml', 'digitata']
+    },
+
     {
         url: '/blog/deploy-a-simple-blog-on-cloudfronts3-with-aws-pipelines-using-cdkcloudformation/',
         title: 'Deploy a simple blog on Cloudfront/S3 with AWS Pipelines using CDK/Cloudformation',
-        publishDate: '30st March 2021',
-        date: '2021-03-30',
+        publishDate: '29th March 2021',
+        date: '2021-03-29',
         excerpt: 'Taking advantage of AWS Free Tier, you can host your own blog for free. Using basic AWS services, it can also auto-update whenever you commit markdown to a GitHub repo.',
         status: 'published',
         readMins: 8,
         tags: [ 'aws', 'personal', 'vue']
+    },
+
+    {
+        url: 'https://medium.com/@nico.kruger_5314/serverless-business-kpi-monitoring-with-aws-opsgenie-and-some-ml-on-the-side-82e8c40e9c2b',
+        link: true,
+        title: 'Serverless Business KPI Monitoring with AWS, Opsgenie and some ML on the side.',
+        publishDate: '29th March 2021',
+        date: '2020-10-24',
+        excerpt: 'How we created a low cost, centralised, pro-active, serverless monitoring system for our business using AWS services and Opsgenie.',
+        status: 'published',
+        readMins: 10,
+        tags: [ 'aws', 'ml', 'digitata']
     }
+
 
 ]
